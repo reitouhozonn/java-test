097741950ee3e5464df157de447d1db57333239d
teionhozonn <teionhozonn@gmail.com>
2021-10-01 05:27:36 +0900

    test ok

--- server/src/main/java/com/example/api/controllers/TestController.java
+++ server/src/main/java/com/example/api/controllers/TestController.java
@@ -8,6 +8,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String test() {
-        return "ok from test.";
+        return "ok from test!!!";
+    }
+
+    @RequestMapping(path = "/test/json", method = RequestMethod.GET)
+    public Example testJson() {
+        var examplePOJO = new Example();
+        examplePOJO.value1 = "foo";
+        examplePOJO.value2 = "bar";
+        return examplePOJO;
+    }
+
+    public class Example {
+        public String value1;
+        public String value2;
     }
 }
