fc4ad4f248b79be1829c88502eda8fb722a15794
teionhozonn <teionhozonn@gmail.com>
2021-10-01 04:54:06 +0900

    test

--- server/src/main/java/com/example/api/controllers/TestController.java
+++ server/src/main/java/com/example/api/controllers/TestController.java
@@ -1,6 +1,6 @@
 package com.example.api.controllers;
 
-import org.springframework.web.bind annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(path = "/test", method = RequestMethod.GET)
     public String test() {
-    return "ok form test...";
+        return "ok from test.";
     }
 }
